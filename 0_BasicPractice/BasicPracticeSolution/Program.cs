3c91f9eee6513d25c303aa38b62a02222f6ed228
Raimundas Banevicius <raima156@yahoo.com>
2019-02-07 10:32:46 +0200

    BasicTest Solution added.

--- 0_BasicPractice/BasicPracticeSolution/Program.cs
+++ 0_BasicPractice/BasicPracticeSolution/Program.cs
@@ -39,9 +39,60 @@ namespace BasicPractice
 
             // - SumNumbersInRange funkcija grazina -123 jeigu 'from' skaicius didesnis uz 'to'.
 
+            PracticeVariables();
+
+            Console.WriteLine(CheckMarkAvarage(1.2f, 3.4f, 5.6f));
+            Console.WriteLine(CheckMarkAvarage(9f, 9f, 9f));
+            Console.WriteLine(CheckMarkAvarage(4f, 5f, 6f));
+
+            Console.WriteLine(SumNumbersInRange(-10, 10));
+            Console.WriteLine(SumNumbersInRange(10, -10));
+
+
             Console.ReadKey();
         }
 
 
+
+        static void PracticeVariables()
+        {
+            int intNumber = 123;
+            float realNumber = 12.34f;
+            string text = "test";
+            bool isItTrue = true;
+
+            Console.WriteLine($"Var test: int:{intNumber} float:{realNumber} string:{text} bool:{isItTrue}");
+        }
+
+        static string CheckMarkAvarage(float mark1, float mark2, float mark3)
+        {
+            if (mark1 < 3.5 || mark2 < 3.5 || mark3 < 3.5)
+            {
+                return "blogai";
+            }
+
+            if (mark1 > 8 && mark2 > 8 && mark3 > 8)
+            {
+                return "Labai puiku!";
+            }
+
+            return "Vidurkis:" + (mark1 + mark2 + mark3) / 3;
+        }
+
+        static int SumNumbersInRange(int from, int to)
+        {
+            if (from > to)
+            {
+                return -123;
+            }
+
+            int sum = 0;
+
+            for (int i = from; i <= to; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
     }
 }
