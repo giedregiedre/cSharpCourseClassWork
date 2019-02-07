935b87f3cfb52bd99fcd3e7b1bd37dcd0d566854
Raimundas Banevicius <raima156@yahoo.com>
2019-02-07 12:25:53 +0200

    ArrayPractice project created.

---
+++ 1_ArrayPractice/ArrayPractice/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array practice!");
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
